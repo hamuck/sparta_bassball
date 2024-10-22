ac25813c7260a9abdfd1f8de22cfb2185c43fddc
박예진 <qkrdpwls2002>
2024-10-22 13:59:12 +0900

    feat : 랜덤숫자 생성기능 추가
    - MakeNumber 클래스에서 HashSet을 사용해 조건에 맞는 랜덤 숫자를 생성
    - App 의 main에서 MakeNumber가 제대로 작동하는지 확인하는 테스트코드 작성
    - 불필요한 구조의 클래스 삭제

--- src/MakeNumber.java
+++ src/MakeNumber.java
@@ -1,2 +1,44 @@
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Random;
+
 public class MakeNumber {
+    private HashSet<Integer> correctNumbers = new HashSet<>();
+    private int correctNumber = 0;
+    Random random = new Random();
+    Iterator iter = correctNumbers.iterator();
+
+    public void makeNumber(int n){
+        correctNumbers.clear();
+        for (int i = 0; i < n; i++) {
+            int inputNumber = random.nextInt(9);
+            if(!correctNumbers.contains(inputNumber)){
+                correctNumbers.add(inputNumber);
+            }
+        }
+    }
+
+    public void setCorrectNumber(){
+        String combineNumber = "";
+        for(Integer i : this.correctNumbers){
+            combineNumber += String.valueOf(i);
+        }
+        this.correctNumber = Integer.valueOf(combineNumber);
+    }
+
+    public int getCorrectNumber(){
+        System.out.println("정답 Int: " + this.correctNumber);
+        return this.correctNumber;
+    }
+
+    public HashSet getCorrectNumbers(){
+        System.out.println("정답 HashSet: "+correctNumbers);
+        return correctNumbers;
+    }
+
+
+
+
 }
